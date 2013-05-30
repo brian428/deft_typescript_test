827ff66b953a8486f8621d39017d651bd769b3ce
Brian Kotek <brian428@gmail.com>
2013-05-30 01:22:57 -0400

    Remove unneeded code in Application.ts

--- app/Application.ts
+++ app/Application.ts
@@ -2,73 +2,53 @@
 /// <reference path="../definitions/DeftJS.d.ts" />
 /// <reference path="./config/AppConfig.ts" />
 
-var __extends = function (d, b) {
-    var temp = true;
-};
+Ext.define("DeftTypeScript.Application", <Deft.mvc.IApplication>{
+    extend: "Deft.mvc.Application",
+    requires: ["DeftTypeScript.config.AppConfig", "DeftTypeScript.view.Viewport"],
 
-module DeftTypeScript {
+    /**
+    * init() runs when Ext.onReady() is called.
+    */
+    init: function () {
+        this.beforeInit();
+        Deft.ioc.Injector.configure(this.buildInjectorConfiguration());
+        return this.afterInit();
+    },
 
-    export class MyApplication extends Deft.mvc.Application {
-        extend: string = "Deft.mvc.Application";
-        requires: string[] = ["DeftTypeScript.config.AppConfig", "DeftTypeScript.view.Viewport"];
+    /**
+    * @protected
+    * Returns the configuration object to pass to Deft.Injector.configure(). Override in subclasses to alter the Injector configuration before returning the config object.
+    * @return {Object} The Injector configuration object.
+    */
+    buildInjectorConfiguration: function() {
+        var config;
         
-        constructor(_super = function () { } ) {
-            super();
-            /*
-            
-            this['constructor'] = function () {
-                this.callParent(arguments);
+        config = {
+            appConfig: {
+                className: "DeftTypeScript.config.AppConfig",
+                parameters: [
+                    {
+                        environment: DeftTypeScript.config.AppConfig.MOCK_DATA_ENV
+                    }
+                ]
             }
-            */
-        }
-
-        /**
-        * init() runs when Ext.onReady() is called.
-        */
-        init = function () {
-            this.beforeInit();
-            Deft.ioc.Injector.configure(this.buildInjectorConfiguration());
-            return this.afterInit();
         };
+        return config;
+    },
 
-        /**
-        * @protected
-        * Returns the configuration object to pass to Deft.Injector.configure(). Override in subclasses to alter the Injector configuration before returning the config object.
-        * @return {Object} The Injector configuration object.
-        */
-        buildInjectorConfiguration = function() {
-            var config;
-            var DeftTypeScript = window['DeftTypeScript'];
-            debugger;
-            config = {
-                appConfig: {
-                    className: "DeftTypeScript.config.AppConfig",
-                    parameters: [
-                      {
-                          environment: DeftTypeScript.config.AppConfig.MOCK_DATA_ENV
-                      }
-                    ]
-                }
-            };
-            return config;
-        };
+    /**
+    * @protected
+    * Runs at the start of the init() method. Override in subclasses if needed.
+    */
+    beforeInit: function() {},
 
-        /**
-        * @protected
-        * Runs at the start of the init() method. Override in subclasses if needed.
-        */
-        beforeInit = function() {};
-
-        /**
-        * @protected
-        * Runs at the end of the init() method. Useful to create initial Viewport, start Jasmine tests, etc.
-        */
-        afterInit = function() {
-            Ext.tip.QuickTipManager.init();
-            return Ext.create("DeftTypeScript.view.Viewport");
-        }
-
+    /**
+    * @protected
+    * Runs at the end of the init() method. Useful to create initial Viewport, start Jasmine tests, etc.
+    */
+    afterInit: function() {
+        Ext.tip.QuickTipManager.init();
+        return Ext.create("DeftTypeScript.view.Viewport");
     }
-}
-var config = new DeftTypeScript.MyApplication();
-Ext.define("DeftTypeScript.Application", config);
+
+});
